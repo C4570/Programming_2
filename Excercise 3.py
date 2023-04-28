f24364db3b1a7b175b95bba55f5ec5dfe29cd7bb
C4570 <julianfrancisco2010@gmail.com>
2023-04-28 15:55:33 -0300

    deleted:    Excercise 10.py deleted:    Excercise 11.py deleted:    Excercise 2.py deleted:    Excercise 3.py deleted:    Excercise 4.py deleted:    Excercise 5.py deleted:    Excercise 6.py deleted:    Excercise 8.py deleted:    Excercise 9.py renamed:    Excersice 7.py -> Exercise 7.py deleted:    Exersice 7.py

--- Excercise 3.py
+++
@@ -1,7 +0,0 @@
-def impriir_numero(n, cont= 1):
-    if n < 10:
-        return cont
-    else:
-        return impriir_numero(n/10, cont+1)
-    
-print(impriir_numero(0))
--- Excercise 5.py
+++
@@ -1,9 +0,0 @@
-def es_potencia(n, b):
-    if n == b:
-        return True
-    elif n < b:
-        return False
-    else:
-        return es_potencia(n/b, b)
-    
-print(es_potencia(16,2))
